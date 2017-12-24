557142e49028d0df1d6502f8daf3b56def9588a9
Stone PEN <pengqiu0815@gmail.com>
2017-12-24 17:29:53 +0800

    first working version.

--- www/js/app.js
+++ www/js/app.js
@@ -1,73 +1,75 @@
-// Ionic Starter App
+angular.module('newsionic', ['ionic', 'newsionic.controllers', 'ionicLazyLoad'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
-
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $ionicPopup) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    console.log('is core:' + $ionicPlatform.is('core'));
+    console.log('is cordova:' + $ionicPlatform.is('cordova'));
+    console.log('is ios:' + $ionicPlatform.is('ios'));
+    console.log('is android:' + $ionicPlatform.is('android'));
+    console.log('is windows:' + $ionicPlatform.is('windows'));
+    console.log('is mobile:' + $ionicPlatform.is('mobile'));
+    console.log('is mobileweb:' + $ionicPlatform.is('mobileweb'));
   });
+  $ionicPlatform.registerBackButtonAction(function(event) {
+    if (true) { // your check here
+      $ionicPopup.confirm({
+        title: 'System warning',
+        template: 'are you sure you want to exit?'
+      }).then(function(res) {
+        if (res) {
+          ionic.Platform.exitApp();
+        }
+      })
+    }
+  }, 100);
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+		url: '/app',
+		abstract: true,
+		templateUrl: 'templates/menu.html',
+		controller: 'NewsCtrl'
+	  })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
+	.state('app.posts', {
+      url: '/posts',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/posts.html',
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+	
+	.state('app.post', {
+		url: "/posts/:postId",
+		views: {
+		  'menuContent': {
+			templateUrl: "templates/post.html",
+			controller: 'PostCtrl'
+		  }
+		}
+	})
+	
+	.state('app.category', {
+		url: "/category/:catSlug",
+		views: {
+		  'menuContent': {
+			templateUrl: "templates/category.html",
+			controller: 'CategoryCtrl'
+		  }
+		}
+	})
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/posts');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,318 @@
-angular.module('starter.controllers', [])
+angular.module('newsionic.controllers', ['ionic'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('NewsCtrl', function($scope, $ionicLoading, $stateParams, $http, $timeout) {
+	api_v1 = {
+		prefix: 'posts/',
+		posts: '',
+		post: '',
+		categories: 'types/posts/taxonomies/category/terms',
+	};
+	api_v2 = {
+		prefix: 'wp/v2/',
+		posts: 'posts/',
+		post: 'posts/',
+		categories: 'categories/',
+		translator: function(src_type, data) {
+			if (src_type == 'posts') return this.translate_posts(data);
+			if (src_type == 'post') return this.translate_post(data);
+			if (src_type == 'categories') return this.translate_categories(data);
+		},
+		translate_post: function(post) {
+			var tr_post = {
+				ID: post.id,
+				title: post.title.rendered,
+				status: 'publish',
+				type: post.type,
+				author: { ID: post.author },
+				content: post.content.rendered,
+				link: post.link,
+				date: post.date,
+				modified: post.modified,
+				format: post.format,
+				slug: post.slug,
+				guid: post.guid.rendered,
+				excerpt: post.excerpt.rendered,
+				menu_order: 0,
+				comment_status: post.comment_status,
+				ping_status: post.ping_status,
+				sticky: post.sticky,
+				date_gmt: post.date_gmt,
+				modified_gmt: post.modified_gmt,
+				meta: {
+					links: {
+						self: post._links.self[0].href,
+						author: post._links.author[0].href,
+						collection: post._links.collection[0].href,
+						replies: post._links.replies[0].href,
+						'version-history': post._links['version-history'][0].href,
+					}
+				},
+				featured_image: {
+					ID: post.featured_media,
+					type: 'attachment',
+					source:'http://localhost:9292/wp-media-v2/' + post._links['wp:featuredmedia'][0].href,
+				},
+			};
+			return tr_post;
+		},
+		translate_posts: function(posts) {
+			var tr_posts = [];
+			for (let p of posts) {
+				var post = {
+					ID: p.id,
+					title: p.title.rendered,
+					status: 'publish',
+					type: p.type,
+					author: { ID: p.author, username: 'unknown', name: 'unknown' },
+					content: p.content.rendered,
+					link: p.link,
+					date: p.date,
+					modified: p.modified,
+					format: p.format,
+					slug: p.slug,
+					guid: p.guid.rendered,
+					excerpt: p.excerpt.rendered,
+					menu_order: 0,
+					comment_status: p.comment_status,
+					ping_status: p.ping_status,
+					sticky: p.sticky,
+					date_tz: 'UTC',
+					date_gmt: p.date_gmt,
+					modified_tz: 'UTC',
+					modified_gmt: p.modified_gmt,
+					meta: {
+						links: {
+							self: p._links.self[0],
+							author: p._links.author[0].href,
+							collection: p._links.collection[0].href,
+							replies: p._links.replies[0].href,
+							'version-history': p._links['version-history'][0].href,
+						}
+					},
+					featured_image: {
+						ID: p.featured_media,
+						type: 'attachment',
+						source: 'http://localhost:9292/wp-media-v2/' + p._links['wp:featuredmedia'][0].href,
+					},
+				};
+				tr_posts.push(post);
+			}//for-of
+			return tr_posts;
+		},
+		translate_categories: function(cats) {
+			var tr_cats = [];
+			for (let c of cats) {
+				var cat = {
+					ID: c.id,
+					name: c.name,
+					slug: c.slug,
+					description: c.description,
+					taxonomy: c.taxonomy,
+					parent: null,
+					count: c.count,
+					link: c.link,
+					meta: {
+						links: {
+							collection: c._links.collection[0].href,
+							self: c._links.self[0].href,
+						}
+					}, // meta
+				};
+				tr_cats.push(cat);
+			} // for-of
+			return tr_cats;
+		}
+	};
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+	// Change your json API url here with http://crossorigin.me/http://yourwordpressurl/wp-json/posts
+	api = api_v2;
+	let cor = 'http://localhost:9292/cor/';
+	let base = 'https://www.villagevoice.com/wp-json/';
 
-  // Form data for the login modal
-  $scope.loginData = {};
+	window.api_url = function(url_type, query) {
+		if (query == undefined) query = null;
+		return $scope.newsAPI + api.prefix + api[url_type] + (query || '');
+	};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+	//$scope.newsAPI = 'http://localhost:9292/cor/http://pixelmarketing.biz/news/wp-json/posts/';
+	$scope.newsAPI = cor + base;
+		
+	$scope.show = function() {
+		$ionicLoading.show({
+			template: 'Loading...'
+		});
+	};
+	
+	$scope.hide = function(){
+		$ionicLoading.hide();
+	};
+	
+	$scope.categories = [];
+	$scope.posts = [];
+	
+	
+	// Scroll to Refresh
+	$scope.doRefresh = function() {
+		$http.get(api_url('posts')).success(function(data){
+			if (api.translator) data = api.translator('posts', data);
+			$scope.posts = data || [];
+			$scope.$broadcast('scroll.refreshComplete');
+		});
+	};
+	
+	//Fetch the Categories list to show in Sliding Menu
+	$http.get(api_url('categories')).success(function(data){
+		if (api.translator) data = api.translator('categories', data);
+		$scope.categories = data || [];
+	});
+	
+	$scope.show();
+	
+	$scope.loadPosts = function() {
+		// Fetch the Latest Posts
+		$http.get(api_url('posts')).success(function(data){
+			if (api.translator) data = api.translator('posts', data);
+			$scope.posts = data || [];
+			window.localStorage.setItem("posts", JSON.stringify(data));
+			$scope.hide();
+		})
+		.error(function(data) {
+            if(window.localStorage.getItem("posts") !== undefined) {
+                $scope.posts = JSON.parse(window.localStorage.getItem("posts"));
+            }
+        });
+	};
+	
+	
+	
+	// Load posts on page load
+    $scope.loadPosts();
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    paged = 2;
+    $scope.moreItems = true;
+	
+		$scope.loadMore = function() {
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+      if( !$scope.moreItems ) {
+        return;
+      }
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+			var pg = paged++;
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+      $timeout(function() {
+
+        $http.get(api_url('posts', '?page=' + pg)).success(function(data, status, headers, config){
+					if (api.translator) data = api.translator('posts', data) || [];
+          angular.forEach( data, function( value, key ) {
+            $scope.posts.push(value);
+          });
+
+          if( data.length <= 0 ) {
+            $scope.moreItems = false;
+          }
+					$scope.$broadcast('scroll.infiniteScrollComplete');
+					$scope.$broadcast('scroll.resize');
+				}).
+        error(function(data, status, headers, config) {
+          $scope.moreItems = false;
+          console.log('error');
+					$scope.$broadcast('scroll.infiniteScrollComplete');
+					$scope.$broadcast('scroll.resize');
+				});
+
+      }, 1000);
+
+    }
+
+    $scope.moreDataExists = function() {
+      return $scope.moreItems;
+    }
+	
+	
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+
+.controller('CategoryCtrl', function($scope, $ionicLoading, $stateParams, $http, $timeout) {
+	
+	var filter = '?filter[category_name]='+$stateParams.catSlug;
+	if (api == api_v2) filter = '?categories=' + $stateParams.catSlug;
+
+	$scope.doRefresh = function() {
+		$http.get(api_url('posts', filter)).success(function(data){
+			if (api.translator) data = api.translator('posts', data) || [];
+			$scope.catPosts = data;
+			$scope.$broadcast('scroll.refreshComplete');
+		});
+	};
+	
+	$scope.catPosts = [];
+	$scope.category = null;
+	for (let c of $scope.categories) {
+		if (c.ID == $stateParams.catSlug) {
+			$scope.category = c;
+			break;
+		}
+	}
+	
+	$scope.show();
+	
+	$http.get(api_url('posts', filter)).success(function(data){
+		if (api.translator) data = api.translator('posts', data) || [];
+		$scope.catPosts = data;
+		$scope.hide();
+	});
+	
+	paged = 2;
+	$scope.moreItems = true;
+
+	$scope.loadMore = function() {
+
+		if( !$scope.moreItems ) {
+			return;
+		}
+
+		var pg = paged++;
+
+		$timeout(function() {
+
+			// var cf = '?filter[category_name]=' + $stateParams.catSlug;
+			// if (api == api_v2) cf = '?categories=' + $stateParams.catSlug;
+			$http.get(api_url('posts', filter + '&page=' + pg)).success(function(data, status, headers, config){
+				if (api.translator) data = api.translator('posts', data) || [];
+				angular.forEach( data, function( value, key ) {
+					$scope.catPosts.push(value);
+				});
+
+				if( data.length <= 0 ) {
+					$scope.moreItems = false;
+				}
+				$scope.$broadcast('scroll.infiniteScrollComplete');
+				$scope.$broadcast('scroll.resize');
+			}).
+			error(function(data, status, headers, config) {
+				$scope.moreItems = false;
+				console.log('error');
+				$scope.$broadcast('scroll.infiniteScrollComplete');
+				$scope.$broadcast('scroll.resize');
+			});
+
+		}, 1000);
+
+	}
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('PostCtrl', function($scope, $stateParams, $sce, $ionicLoading, $http ) {
+
+	
+	$scope.post = [];
+
+	$scope.show();
+
+	$http.get(api_url('post', $stateParams.postId)).success(function(data){
+		if (api.translator) data = api.translator('post', data) || [];
+		$scope.post = data;
+
+		$scope.hide();
+	});
+ 
+})
